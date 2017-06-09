0a4b70c357e0ee953591458d368dab20758a98f5
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2017-06-09 21:23:13 +0300

    More fixes regarding to indentation.

--- include/game.h
+++ include/game.h
@@ -1,9 +1,9 @@
 /*! The game instance for the whole application.
 
-	This is the main instance for the whole Pong game application. This class
-	encapsulates the required functionality to tie the application as a whole
-	by combining and providing a way to manage the game state and scenes as 
-	well as the management of performing logical game updates (i.e. ticks).
+  This is the main instance for the whole Pong game application. This class
+  encapsulates the required functionality to tie the application as a whole
+  by combining and providing a way to manage the game state and scenes as 
+  well as the management of performing logical game updates (i.e. ticks).
  */
 #ifndef PONG_GAME_H
 #define PONG_GAME_H
